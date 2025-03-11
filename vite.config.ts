a09029cccd65e3a47365ffa9f70ac4cc48bcb924
K.Matsu <rbl.okcomputer@outlook.com>
2025-03-11 20:45:57 +0900

    manifest.jsonの設定、metaタグの設定

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,35 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
+      },
+      manifest: {
+        name: "EML VIEWER",
+        short_name: "EML VIEWER",
+        theme_color: "#ffffff",
+        description: "EMLファイルのビューアーです。",
+        "icons": [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
       }
     })
   ],
